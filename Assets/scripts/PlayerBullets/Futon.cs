7f0d1add1cc13f8e2604ade87be13b1cf4dc707b
s18014 <s18014@std.it-college.ac.jp>
2018-09-16 05:10:30 +0900

    test

---
+++ Assets/scripts/PlayerBullets/Futon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Futon : MonoBehaviour {
+    public float power;
+    public float angryPoint;
+    public float speed;
+    public float angle;
+    public float coolTime;
+    public float gravity;
+    float vx = 0f;
+    float vy = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+        vx = Mathf.Cos(Mathf.Deg2Rad * angle) * speed;
+        vy = Mathf.Sin(Mathf.Deg2Rad * angle) * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void FixedUpdate()
+    {
+        vy -= gravity * 9.81f * Time.deltaTime;
+        Vector2 pos = transform.position;
+        pos.x += vx * Time.deltaTime;
+        pos.y += vy * Time.deltaTime;
+        transform.position = pos;
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D c)
+    {
+        string layerName = LayerMask.LayerToName(c.gameObject.layer);
+        if (layerName == "DestroyArea")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/PlayerBullets/Zabuton.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zabuton : MonoBehaviour {
+    public float power;
+    public float angryPoint;
+    public float speed;
+    public float coolTime;
+    public float angle;
+    bool isOnGround;
+    Vector2 min;
+    Vector2 size;
+
+
+	// Use this for initialization
+	void Start () {
+        min = Camera.main.ViewportToWorldPoint(Vector2.zero);
+        size = GetComponent<SpriteRenderer>().bounds.size / 2f;
+        isOnGround = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        onGround();
+        Vector2 pos = transform.position;
+        if (isOnGround)
+        {
+            pos.y = min.y + size.y;
+            pos.x += speed * Time.deltaTime * 3f;
+        } else
+        {
+            pos.x += Mathf.Cos(Mathf.Deg2Rad * angle) * Time.deltaTime * speed;
+            pos.y += Mathf.Sin(Mathf.Deg2Rad * angle) * Time.deltaTime * speed;
+        }
+        transform.position = pos;
+		
+	}
+
+    private void OnTriggerExit2D(Collider2D c)
+    {
+        string layerName = LayerMask.LayerToName(c.gameObject.layer);
+        if (layerName == "DestroyArea")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void onGround ()
+    {
+        if (min.y >= transform.position.y - size.y) isOnGround = true;
+    }
+}
